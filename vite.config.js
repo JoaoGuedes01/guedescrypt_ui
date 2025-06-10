3775ee4b1ede5c2714fb2e5a330c6c83394b68e1
joaoguedes <joaoguedes.cjp@gmail.com>
2025-06-10 15:50:12 +0100

    idk
    idk

--- vite.config.js
+++ vite.config.js
@@ -6,23 +6,15 @@ export default defineConfig({
   plugins: [
     react(),
     nodePolyfills({
-      // To add only specific polyfills, add them here. If no option is passed, adds all polyfills
-      // include: ['crypto', 'stream'], 
-      // To exclude specific polyfills, add them to this list. Note: if include is provided, this has no effect
-      // exclude: ['http'],
-      // Whether to polyfill specific globals.
       globals: {
         Buffer: true, // polyfill Buffer
         global: true, // polyfill global
         process: true, // polyfill process
       },
-      // Whether to polyfill `node:` protocol imports.
       protocolImports: true,
     }),
   ],
   base: '/guedescrypt_ui/',
-  // If you are experiencing issues with the `Buffer` global, you might need to
-  // explicitly define it in `optimizeDeps.esbuildOptions.define` as well.
   optimizeDeps: {
     esbuildOptions: {
       define: {
